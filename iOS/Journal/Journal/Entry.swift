2210060daf1198a6c665b72d9d59429fdaa5546d
Sandrawar <Sandra.A3@o2.pl>
2021-04-11 09:11:05 +0200

    Multiple Screens and navigation added

---
+++ iOS/Journal/Journal/Entry.swift
@@ -0,0 +1,15 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Ariel-M-01 on 11/04/2021.
+//
+
+import Foundation
+
+struct Entry {
+    let date: Date
+    var text: String
+    let title: String
+    var inspiration: String?
+}
