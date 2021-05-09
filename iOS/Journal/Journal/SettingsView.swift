99dd85f47df4cb1ff508d1ee50fc8ac812ebc852
sandrawar <Sandra.A3@o2.pl>
2021-05-09 14:38:16 +0200

    Journal: privacy policy

--- iOS/Journal/Journal/SettingsView.swift
+++ iOS/Journal/Journal/SettingsView.swift
@@ -10,61 +10,67 @@ struct SettingsView: View {
     @ObservedObject var userSettings: UserSettings
 
     var body: some View {
-    VStack {
-        Text("settings").fontWeight(.bold)
-            .font(.largeTitle)
-        List {
-            HStack {
-                Image(systemName: "paintpalette")
-                Text("settings-colour").fontWeight(.bold)
-            }
-            Button(action: {
-                userSettings.colorScheme = .light
-            }) {
+        VStack {
+            Text("settings").fontWeight(.bold)
+                .font(.largeTitle)
+            List {
                 HStack {
-                    Text("light-schema")
-                    Spacer()
-                    if userSettings.colorScheme == .light {
-                        selectedImage
+                    Image(systemName: "paintpalette")
+                    Text("settings-colour").fontWeight(.bold)
+                }
+                Button(action: {
+                    userSettings.colorScheme = .light
+                }) {
+                    HStack {
+                        Text("light-schema")
+                        Spacer()
+                        if userSettings.colorScheme == .light {
+                            selectedImage
+                        }
                     }
                 }
-            }
-            Button(action: {
-                userSettings.colorScheme = .dark
-            }) {
-                HStack {
-                    Text("dark-schema")
-                    Spacer()
-                    if userSettings.colorScheme == .dark {
-                        selectedImage
+                Button(action: {
+                    userSettings.colorScheme = .dark
+                }) {
+                    HStack {
+                        Text("dark-schema")
+                        Spacer()
+                        if userSettings.colorScheme == .dark {
+                            selectedImage
+                        }
                     }
                 }
-            }
-            Button(action: {
-                userSettings.colorScheme = .none
-            }) {
-                HStack {
-                    Text("system-scheme")
-                    Spacer()
-                    if userSettings.colorScheme == .none {
-                        selectedImage
+                Button(action: {
+                    userSettings.colorScheme = .none
+                }) {
+                    HStack {
+                        Text("system-scheme")
+                        Spacer()
+                        if userSettings.colorScheme == .none {
+                            selectedImage
+                        }
                     }
                 }
-            }
 
-            Spacer()
-            Link(destination: URL(string: UIApplication.openSettingsURLString)!){
-                HStack {
-                    Image(systemName: "quote.bubble")
-                    Text("language-settings")
-                        .fontWeight(.bold)
+                Spacer()
+                Link(destination: URL(string: UIApplication.openSettingsURLString)!){
+                    HStack {
+                        Image(systemName: "quote.bubble")
+                        Text("language-settings")
+                            .fontWeight(.bold)
+                    }
                 }
             }
+            .listStyle(InsetGroupedListStyle())
+            
+            Link(destination: URL(string: "https://sandrawar.github.io/PublicInfo/JournalPrivacyPolicy")!, label: {
+                Text("Privacy Policy")
+            })
+            .padding()
+            Spacer()
         }
-        .listStyle(InsetGroupedListStyle())
         .preferredColorScheme(userSettings.colorScheme)
         .navigationBarTitle("settings")
-    }
     }
     var selectedImage: some View {
         Image(systemName: "checkmark")
